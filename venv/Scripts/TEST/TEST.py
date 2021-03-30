f38c53641a644b1c15065317698042d1917db0ae
mhaitov <81020414+mhaitov@users.noreply.github.com>
2021-03-30 21:07:01 +0300

    TEST

---
+++ venv/Scripts/TEST/TEST.py
@@ -0,0 +1,51 @@
+ ###1
+name = 'Marian'
+surname ='Khaitov'
+age = '24'
+print('Hello',surname, name, 'you are', age, 'old')
+
+####2
+a = 10
+b = 20
+c = 30
+print(a,b,c)
+half_perimeter = (int(a) +int(b) +int(c)) / 2
+print(half_perimeter)
+
+triangle_area = (half_perimeter * (half_perimeter - int(a)) * (half_perimeter-int(b)) * (half_perimeter - int(c)))
+print(triangle_area)
+
+### 3
+
+#### 4
+some_string = 'Hello world'
+print(some_string)
+print(some_string[::-1])
+
+
+#####5
+numbers = ['1','2','3','4','5']
+print(len(numbers))
+numbers.insert(2, '8, 6, 9')
+print(numbers)
+
+##### 6
+numbers = {'1', '2', '3', '4', '7', '9', '9'}
+numbers2 = {'1', '2', '4', '6', '4', '6'}
+#
+# print(numbers)
+# print(numbers2)
+
+print(numbers.intersection(numbers2))
+print(numbers.difference(numbers2))
+print(numbers2.union(numbers))
+
+
+#### 7
+import datetime
+
+t = (29,6,1996)
+print(t)
+
+dt = datetime.datetime(1996, 6, 29, 15, 20, 20)
+print(dt)
